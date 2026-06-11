21516d479a11312d6cd4be4eda667eac5d3bd9b7
kevern <sander.langens@gmail.com>
2026-06-11 20:57:57 +0200

    initialized basic pyside6 functionality

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from PySide6 import QtCore, QtGui, QtWidgets
+
+class Randotron(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Randotron, self).__init__()
+
+        self.centralWidget = QtWidgets.QWidget()
+        self.setCentralWidget(self.centralWidget)
+
+        # create widgets to add to layout
+        self.button = QtWidgets.QPushButton("Randotron Activate")
+        self.text = QtWidgets.QLabel("RANDOTRON", alignment=QtCore.Qt.AlignCenter)
+
+        # create layout and add widgets
+        self.layout = QtWidgets.QVBoxLayout(self.centralWidget)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        # set layout
+        self.setLayout(self.layout)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app = QtWidgets.QApplication([])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    widget = Randotron()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
