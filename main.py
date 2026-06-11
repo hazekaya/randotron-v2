1b4e77d60f9d86fe0906ac2447b85119d67b1e67
kevern <sander.langens@gmail.com>
2026-06-11 21:18:11 +0200

    added basic RNG movie picker funtionality with requests and random modules

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+import random
 import sys
+import urllib
 
+import requests
 from PySide6 import QtCore, QtGui, QtWidgets
+
+JELLYFIN_KEY = "e9e4559fe74a4466b197b0955355cf67"
+JELLYFIN_URL = "http://192.168.0.253:8096"
+JELLYFIN_USER = "aa01a24ed1344f6993d1176fff245d14"
+API_URL = f"{JELLYFIN_URL}/Users/{JELLYFIN_USER}/Items?IncludeItemTypes=Movie&Filters=IsUnplayed&Recursive=true&Fields=Path"
+HEADERS = {
+    "X-Emby-Token": JELLYFIN_KEY,
+    "Accept": "application/json"
+}
 
 class Randotron(QtWidgets.QMainWindow):
     def __init__(self):
@@ -20,6 +32,32 @@ class Randotron(QtWidgets.QMainWindow):
 
         # set layout
         self.setLayout(self.layout)
+
+        self.button.clicked.connect(self.randotron)
+
+    def randotron(self):
+        movie_list = []
+        print(API_URL)
+
+        try:
+            response = requests.get(url=API_URL, headers=HEADERS)
+            response.raise_for_status()
+
+            data = response.json()
+            movies = data.get("Items", [])
+
+            if movies is not None:
+                for movie in movies:
+                    title = movie.get("Name")
+
+                    # print(movie)
+                    movie_list.append(title)
+
+        except requests.exceptions.HTTPError as err:
+            print(err)
+
+        print(random.choice(movie_list))
+
 
 
 if __name__ == '__main__':
